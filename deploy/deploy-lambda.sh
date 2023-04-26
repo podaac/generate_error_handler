23033810ceb0cdff73c2fd79cf22d79520772c5c
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-04-26 14:16:08 +0000

    Initial GitHub Actions workflow

---
+++ deploy/deploy-lambda.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Script to create a zipped deployment package for a Lambda function.
+#
+# Command line arguments:
+# [1] app_name: Name of application to create a zipped deployment package for
+# 
+# Example usage: ./delpoy-lambda.sh "my-app-name"
+
+APP_NAME=$1
+ROOT_PATH="$PWD"
+echo "ROOT_PATH: $ROOT_PATH"
+$ZIP_PATH=$ROOT_PATH/$APP_NAME.zip
+$APP_PATH=$ROOT_PATH/$APP_NAME.py
+
+zip $ZIP_PATH $APP_PATH
+echo "Created: $ZIP_PATH."
